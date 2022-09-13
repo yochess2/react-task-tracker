91a22bb11114a11001a14a574cbdb2bea44605d2
unknown <drakewang@gmail.com>
2022-09-12 17:26:58 -0700

    Finish Delete and Toggle

--- src/App.js
+++ src/App.js
@@ -24,10 +24,22 @@ function App() {
   }
 ])
 
+// Delete task
+const deleteTask = (id) => {
+  console.log('delete', id)
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  console.log('toggle', id)
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
       <Header title='Task Tracker' />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show '}
     </div>
   );
 }
