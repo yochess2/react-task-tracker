a5e67f000080f85d864713ca79965485720e4290
unknown <drakewang@gmail.com>
2022-09-14 23:13:17 -0700

    Add dynamic color to addtask

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header title='Task Tracker' onAdd={() => {setShowAddTask(!showAddTask)}}/>
+      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
       {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show '}
     </div>
