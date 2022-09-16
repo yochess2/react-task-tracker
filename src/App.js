140a1de3c79ddc86fabc27990906bcc849470aa3
unknown <drakewang@gmail.com>
2022-09-15 21:25:10 -0700

    Finish crash course

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(true)
@@ -78,11 +82,14 @@ const toggleReminder = async (id) => {
 }
 
   return (
-    <div className="container">
-      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show '}
-    </div>
+    <Router>
+      <div className="container">
+        <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask}/>}
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show '}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
